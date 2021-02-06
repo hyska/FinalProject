e2c1b984f6683ba67cca5a3f82422920b4f285ff
hyska <hayriyeyasak1@gmail.com>
2021-02-07 00:23:19 +0300

    06.02.2020 00.30

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +8,11 @@ using System.Text;
 namespace DataAccess.Abstract
 {
     //interface default public değildir methodları operasyonları publictir
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IProductDal : IEntityRepository<Product>
     {
-        
 
-    } 
+        List<ProductDetailDto> GetProductDetails();
+    }
+         
 }
 //Code Refactoring
---
+++ SQLQueryTest.sql
@@ -0,0 +1,4 @@
+﻿select ProductID, ProductName, CategoryName, UnitPrice
+from Products
+inner join Categories
+on Products.CategoryID = Categories.CategoryID
