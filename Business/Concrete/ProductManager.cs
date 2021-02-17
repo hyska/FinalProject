62b4efb8c70f9d98e813a5ee4674663fe5bd65cc
hyska <hayriyeyasak1@gmail.com>
2021-02-17 23:10:33 +0300

    17.02.2021 Fluent Validation

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,11 +25,26 @@ namespace Business.Concrete
         public IResult Add(Product product)
         {
             //business codes
-            if (product.ProductName.Length < 2)
-            {   //magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-           __productDal.Add(product);
+            //validation - eklenmesini istediğimiz nesne 
+            //ile ilgili yapılar 
+
+            //if (product.UnitPrice <= 0)
+            //{
+            //    return new ErrorResult(Messages.UnitPriceInvalid);
+            //}
+            //if (product.ProductName.Length < 2)
+            //{   //magic strings
+            //    return new ErrorResult(Messages.ProductNameInvalid);
+            //}
+
+            //validation
+
+            ValidationTool.Validate(new ProductValidator(), product);  
+           
+            //Loglama
+           //cacheremove
+
+            __productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
 
